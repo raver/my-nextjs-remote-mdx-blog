68db6ba620c722bd3ab67dee13206b1ed26ffbb6
JudeYu <yetiscream@163.com>
2023-07-07 10:41:15 +0800

    Changed photo and removed some ignored files.

--- src/app/components/MyProfilePic.tsx
+++ src/app/components/MyProfilePic.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function MyProfilePic() {
   return (
     <section className="w-full mx-auto" >
-      <Image src='/images/profile-photo-600x600.png' 
+      <Image src='/images/profile-photo-400x400.jpg' 
       className="border-4 border-black dark:border-slate-500
       drop-shadow-xl shadow-black rounded-full mx-auto mt-8
       
